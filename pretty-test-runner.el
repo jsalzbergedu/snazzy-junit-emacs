cb002dc769575d8f395ed49d59a4e24a0fb6631e
Jacob Salzberg <jssalzbe@ncsu.edu>
2018-12-23 21:20:27 -0500

    Begin pretty-test-runner

---
+++ pretty-test-runner.el
@@ -0,0 +1,30 @@
+;;; pretty-test-runner.el --- Run and display tests in a visually appealing format -*- lexical-binding: t -*-
+
+;; Copyright © 2018 Jacob Salzberg
+
+;; Author: Jacob Salzberg <jssalzbe@ncsu.edu>
+;; URL: https://github.com/jsalzbergedu/pretty-test-runner-emacs
+;; Version: 0.1.0
+;; Keywords: pretty test runner junit java
+
+;; This file is not a part of GNU Emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+;;; pretty-test-runner.el ends here
